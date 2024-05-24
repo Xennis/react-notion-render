3d63b6b32db56d3b58b4d5ab40fe64a8f58230ed
Xennis <code@xennis.org>
2024-05-24 03:48:04 +0200

    Create skeleton for repo

---
+++ examples/nextjs/src/app/page.tsx
@@ -0,0 +1,9 @@
+import {helloWorld} from "@react-notion-render/render";
+
+export default function Home() {
+  return (
+    <main>
+      {helloWorld("hi")}
+    </main>
+  );
+}
---
+++ packages/render/src/index.ts
@@ -0,0 +1,3 @@
+export function helloWorld(name: string) {
+    return `hello ${name}`
+}
