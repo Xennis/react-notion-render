72804a0af55468aba9489d38f92dafbf85c12473
Xennis <code@xennis.org>
2024-05-24 11:22:24 +0200

    Add style

--- examples/nextjs/src/app/page.tsx
+++ examples/nextjs/src/app/page.tsx
@@ -1,6 +1,8 @@
 import {fetchBlocksChildren, Render} from "@react-notion-render/render";
 import {Client} from "@notionhq/client";
 
+import "@react-notion-render/render/styles.css"
+
 const notionClient = new Client({
   auth: process.env.NOTION_ACCESS_TOKEN,
 })
@@ -8,7 +10,7 @@ const notionClient = new Client({
 export default async function Home() {
   const blocks = await fetchBlocksChildren(notionClient, process.env.NOTION_BLOCK_ID!)
   return (
-    <main>
+    <main className="max-w-screen-lg mx-auto">
       <Render blocks={blocks} />
     </main>
   );
