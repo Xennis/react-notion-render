298d0eebb35a82988a339b8f82af7758ed09a9f3
Xennis <code@xennis.org>
2024-05-27 12:47:26 +0200

    [examples] Set render options

--- examples/nextjs/src/app/page.tsx
+++ examples/nextjs/src/app/page.tsx
@@ -6,12 +6,21 @@ import "@react-notion-cms/render/styles.css"
 const notionClient = new Client({
   auth: process.env.NOTION_ACCESS_TOKEN,
 })
+const demoLocale = "en_US"
+
+const formatDateFn = (date: Date) => {
+  return date.toLocaleDateString(demoLocale, {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  })
+}
 
 export default async function Home() {
   const blocks = await fetchBlocksChildren(notionClient, process.env.NOTION_BLOCK_ID!)
   return (
     <main className="mx-auto max-w-screen-lg">
-      <Render blocks={blocks} />
+      <Render blocks={blocks} options={{ formatDateFn: formatDateFn, resolveLinkFn: (nId) => nId }} />
     </main>
   )
 }
