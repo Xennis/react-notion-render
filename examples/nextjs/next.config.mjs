663bd7dd8bf6149cd5878fb08e1da20e1561bce0
Xennis <code@xennis.org>
2024-05-28 23:05:12 +0200

    [examples] Add note to Next.js config reading GitHub Pages

--- examples/nextjs/next.config.mjs
+++ examples/nextjs/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Just set for GitHub Page: https://<username>.github.io/<basePath>/
   basePath: "/react-notion-render",
   output: "export",
 }
