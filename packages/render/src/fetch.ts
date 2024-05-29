3590da300bf8c5841d961eb42b0df73fe27996ad
Xennis <code@xennis.org>
2024-05-29 02:33:42 +0200

    [render] Download internal images

--- packages/render/src/fetch.ts
+++ packages/render/src/fetch.ts
@@ -1,13 +1,24 @@
 import { type Client, isFullBlock, iteratePaginatedAPI } from "@notionhq/client"
 import { BlockObjectResponseWithChildren } from "./types"
+import { type ListBlockChildrenParameters } from "@notionhq/client/build/src/api-endpoints"
 
-export const fetchBlocksChildren = async (client: Client, blockId: string) => {
+type ResolveImageFn = (url: string, meta: { blockId: string; lastEditedTime: Date }) => Promise<string>
+
+export const fetchBlocksChildren = async (
+  client: Client,
+  firstPageArgs: ListBlockChildrenParameters,
+  options: { resolveImageFn?: ResolveImageFn },
+) => {
   const result: Array<BlockObjectResponseWithChildren> = []
-  for await (const block of iteratePaginatedAPI(client.blocks.children.list, {
-    block_id: blockId,
-    page_size: 100,
-  })) {
+  for await (const block of iteratePaginatedAPI(client.blocks.children.list, firstPageArgs)) {
     if (isFullBlock(block)) {
+      if (options.resolveImageFn && block.type === "image" && block.image.type === "file") {
+        block.image.file.url = await options.resolveImageFn(block.image.file.url, {
+          blockId: block.id,
+          lastEditedTime: new Date(block.last_edited_time),
+        })
+      }
+
       if (!block.has_children) {
         result.push(block)
         continue
@@ -19,10 +30,17 @@ export const fetchBlocksChildren = async (client: Client, blockId: string) => {
 
       result.push({
         ...block,
-        _children: await fetchBlocksChildren(client, childId),
+        _children: await fetchBlocksChildren(
+          client,
+          {
+            ...firstPageArgs,
+            block_id: childId,
+          },
+          options,
+        ),
       })
     }
   }
-  console.info(`fetched children from notion block ${blockId}`)
+  console.info(`fetched children from notion block ${firstPageArgs.block_id}`)
   return result
 }
