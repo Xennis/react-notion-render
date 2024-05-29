9ed5339ff73e9d8bb8012c3ef278e2e76cfb95e8
Xennis <code@xennis.org>
2024-05-29 16:36:13 +0200

    Improve support for dark mode and RTL

--- examples/nextjs/src/app/layout.tsx
+++ examples/nextjs/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google"
 import "./globals.css"
+import { classNames } from "@react-notion-cms/render/dist/util"
+import { ToggleDarkModeButton, ToggleRtlDirectionButton } from "@/components/toggle-button"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -10,8 +12,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className} suppressHydrationWarning>
-        {children}
+      <body className={classNames(inter.className, "dark:bg-slate-800")} suppressHydrationWarning>
+        <header className="w-full bg-teal-500">
+          <div className="mx-auto max-w-screen-lg py-2">
+            <div className="flex gap-4">
+              <ToggleDarkModeButton />
+              <ToggleRtlDirectionButton />
+            </div>
+          </div>
+        </header>
+        <main>{children}</main>
       </body>
     </html>
   )
---
+++ examples/nextjs/src/components/toggle-button.tsx
@@ -0,0 +1,47 @@
+"use client"
+
+import { useState } from "react"
+import { classNames } from "@react-notion-cms/render/dist/util"
+
+const Button = ({ className, ...props }: React.ComponentPropsWithoutRef<"button">) => {
+  return (
+    <button
+      className={classNames("rounded bg-black px-2 py-1 text-sm font-semibold text-white", className ?? "")}
+      {...props}
+    >
+      {props.children}
+    </button>
+  )
+}
+
+export const ToggleDarkModeButton = () => {
+  const [darkMode, setDarkMode] = useState(false)
+
+  const handleClick = () => {
+    const enabled = !darkMode
+    setDarkMode(enabled)
+    if (enabled) {
+      document.querySelector("html")?.classList.add("dark")
+    } else {
+      document.querySelector("html")?.classList.remove("dark")
+    }
+  }
+
+  return <Button onClick={handleClick}>{darkMode ? "Light" : "Dark"} Mode</Button>
+}
+
+export const ToggleRtlDirectionButton = () => {
+  const [rtlDirection, setRtlDirection] = useState(false)
+
+  const handleClick = () => {
+    const enabled = !rtlDirection
+    setRtlDirection(enabled)
+    if (enabled) {
+      document.body.style.direction = "rtl"
+    } else {
+      document.body.style.direction = "ltr"
+    }
+  }
+
+  return <Button onClick={handleClick}>{rtlDirection ? "LTR" : "RTL"} Direction</Button>
+}
