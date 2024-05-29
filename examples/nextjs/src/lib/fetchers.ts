be6e02756799fcb938659a7b068dc82745c3672c
Xennis <code@xennis.org>
2024-05-29 02:33:42 +0200

    [render] Download internal images

--- examples/nextjs/src/lib/fetchers.ts
+++ examples/nextjs/src/lib/fetchers.ts
@@ -1,6 +1,8 @@
 import { unstable_cache } from "next/cache"
 import { fetchBlocksChildren } from "@react-notion-cms/render"
 import { Client } from "@notionhq/client"
+import { statSync, writeFileSync } from "node:fs"
+import nextConfig from "../../next.config.mjs"
 
 const notionClient = new Client({
   auth: process.env.NOTION_ACCESS_TOKEN,
@@ -8,7 +10,16 @@ const notionClient = new Client({
 
 export const getCachedPageContent = unstable_cache(
   async (blockId: string) => {
-    return fetchBlocksChildren(notionClient, blockId)
+    return fetchBlocksChildren(
+      notionClient,
+      {
+        block_id: blockId,
+        page_size: 100,
+      },
+      {
+        resolveImageFn: downloadImageToPublicDir,
+      },
+    )
   },
   ["cms-page"],
   // {
@@ -16,3 +27,30 @@ export const getCachedPageContent = unstable_cache(
   //   tags: ["cms-page"],
   // },
 )
+
+const downloadImageToPublicDir = async (url: string, meta: { blockId: string; lastEditedTime: Date }) => {
+  const fileUrl = new URL(url)
+  const originalFileName = fileUrl.pathname.substring(fileUrl.pathname.lastIndexOf("/") + 1)
+  const newFileName = `public/cms/${meta.blockId}-${originalFileName}`
+
+  let savedLastEditedTime: Date | null = null
+  try {
+    const stat = statSync(newFileName)
+    savedLastEditedTime = stat.mtime
+  } catch (error) {
+    if (error instanceof Error && "code" in error && error.code === "ENOENT") {
+      console.debug(`${newFileName} not found`)
+    } else {
+      console.warn(`${newFileName}: ${error}`)
+    }
+  }
+  // Avoid download the file again and again
+  if (savedLastEditedTime === null || meta.lastEditedTime > savedLastEditedTime) {
+    const resp = await fetch(fileUrl)
+    const blob = await resp.blob()
+    writeFileSync(newFileName, new DataView(await blob.arrayBuffer()))
+    console.info(`downloaded image ${newFileName} of block ${meta.blockId}`)
+  }
+
+  return newFileName.replace("public", nextConfig.basePath ?? "")
+}
