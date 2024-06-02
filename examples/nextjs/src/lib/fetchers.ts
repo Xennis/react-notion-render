dc9e532d395d400af790c7d8461ba7cfb10aa5c5
Xennis <code@xennis.org>
2024-06-02 22:41:19 +0200

    [fetch] Move download image function into package

--- examples/nextjs/src/lib/fetchers.ts
+++ examples/nextjs/src/lib/fetchers.ts
@@ -1,6 +1,6 @@
 import { unstable_cache } from "next/cache"
 import { fetchBlocksChildren, IconResponse } from "@react-notion-cms/render"
-import { fetchDatabasePages, propsPlainTexts } from "@react-notion-cms/fetch"
+import { downloadImage, fetchDatabasePages, propsPlainTexts } from "@react-notion-cms/fetch"
 import { Client } from "@notionhq/client"
 import { statSync, writeFileSync } from "node:fs"
 import type { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints"
@@ -61,28 +61,6 @@ export const getCachedPageContent = unstable_cache(
 )
 
 const downloadImageToPublicDir = async (url: string, meta: { blockId: string; lastEditedTime: Date }) => {
-  const fileUrl = new URL(url)
-  const originalFileName = fileUrl.pathname.substring(fileUrl.pathname.lastIndexOf("/") + 1)
-  const newFileName = `public/cms/${meta.blockId}-${originalFileName}`
-
-  let savedLastEditedTime: Date | null = null
-  try {
-    const stat = statSync(newFileName)
-    savedLastEditedTime = stat.mtime
-  } catch (error) {
-    if (error instanceof Error && "code" in error && error.code === "ENOENT") {
-      console.debug(`${newFileName} not found`)
-    } else {
-      console.warn(`${newFileName}: ${error}`)
-    }
-  }
-  // Avoid download the file again and again
-  if (savedLastEditedTime === null || meta.lastEditedTime > savedLastEditedTime) {
-    const resp = await fetch(fileUrl)
-    const blob = await resp.blob()
-    writeFileSync(newFileName, new DataView(await blob.arrayBuffer()))
-    console.info(`downloaded image ${newFileName} of block ${meta.blockId}`)
-  }
-
-  return newFileName.replace("public", nextConfig.basePath ?? "")
+  const localImage = await downloadImage("public/cms", url, meta)
+  return localImage.replace("public", nextConfig.basePath ?? "")
 }
--- packages/fetch/src/index.ts
+++ packages/fetch/src/index.ts
@@ -1,2 +1,3 @@
 export * from "./fetch"
+export * from "./image"
 export * from "./properties"
