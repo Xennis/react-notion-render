577f7e84095389947723ea7950bcadaad150c665
Xennis <code@xennis.org>
2024-06-02 21:09:11 +0200

    [render] Render page title correctly

--- packages/render/src/components/page-title.tsx
+++ packages/render/src/components/page-title.tsx
@@ -3,9 +3,16 @@ import { Icon } from "./icon"
 
 export const PageTitle = ({ icon, children }: { icon: IconResponse; children: React.ReactNode }) => {
   return (
-    <span className="notion-page-title">
-      <Icon icon={icon} width={20} className="notion-page-title-icon" />
-      <span className="notion-page-title-text">{children}</span>
+    // ref: .notion-page-title
+    <span className="inline-flex max-w-full items-center leading-[1.3] transition-[background] delay-[0s] duration-[120ms] ease-[ease-in]">
+      {/* ref: .notion-page-title-icon */}
+      {/* note: plus ref .notion-page-icon-inline for margins */}
+      <Icon icon={icon} width={20} className="me-1.5 ms-0.5 flex shrink-0 items-center justify-center rounded-[3px]" />
+      {/* ref: .notion-page-title-text */}
+      {/* note: Consider how to use the border-buttom here in combination with a link. See also `.notion-link .notion-page-title-text` */}
+      <span className="relative top-px overflow-hidden text-ellipsis whitespace-nowrap border-b border-solid border-b-[color:var(--fg-color-1)] font-medium leading-[1.3]">
+        {children}
+      </span>
     </span>
   )
 }
--- packages/render/src/util.ts
+++ packages/render/src/util.ts
@@ -4,6 +4,8 @@ import { type CSSProperties } from "react"
 // Just a helper type because it's not directly exposed.
 type Color = ParagraphBlockObjectResponse["paragraph"]["color"]
 
+export const notionUrl = "https://www.notion.so"
+
 export const notionColor = (color: Color, backgroundCallout?: boolean): CSSProperties => {
   if (color.endsWith("_background")) {
     return { backgroundColor: `var(--notion-${color}${backgroundCallout ? "_co" : ""}` }
@@ -11,12 +13,11 @@ export const notionColor = (color: Color, backgroundCallout?: boolean): CSSPrope
   return { color: `var(--notion-${color})` }
 }
 
-export const relativeNotionUrl = (url: string) => {
-  return url.replace("https://www.notion.so", "")
-}
-
 // export const noopResolveLinkFn = (nId: string) => nId
 
 export const cn = (...classes: string[]) => {
   return classes.filter(Boolean).join(" ")
 }
+
+export const idToUuid = (id: string) =>
+  `${id.substring(0, 8)}-${id.substring(8, 12)}-${id.substring(12, 16)}-${id.substring(16, 20)}-${id.substring(20)}`
