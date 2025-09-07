35639fd28f8e5ea1177a4b925748953ab7f7fe7b
Xennis <code@xennis.org>
2025-09-07 20:19:42 +0200

    chore: @notionhq/client v5 adjustments

--- packages/fetch/src/fetch.ts
+++ packages/fetch/src/fetch.ts
@@ -1,13 +1,13 @@
 import { type Client, isFullPage, iteratePaginatedAPI } from "@notionhq/client"
-import { type PageObjectResponse, type QueryDatabaseParameters } from "@notionhq/client/build/src/api-endpoints"
+import type { PageObjectResponse, QueryDataSourceParameters } from "@notionhq/client/build/src/api-endpoints"
 
 export const fetchDatabasePages = async <T>(
   client: Client,
   processFn: (page: PageObjectResponse) => Promise<T | null>,
-  firstPageArgs: QueryDatabaseParameters,
+  firstPageArgs: QueryDataSourceParameters,
 ): Promise<Array<T>> => {
   const result: Array<T> = []
-  for await (const item of iteratePaginatedAPI(client.databases.query, firstPageArgs)) {
+  for await (const item of iteratePaginatedAPI(client.dataSources.query, firstPageArgs)) {
     if (isFullPage(item)) {
       const fnResult = await processFn(item)
       if (fnResult !== null) {
